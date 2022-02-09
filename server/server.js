11f787e32eaf1f51dbd0f411863ea9d3ef03fde7
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-09 13:17:26 -0600

    set up template for lab app

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const exports = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public/index.html"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`server running on ${port}`));
